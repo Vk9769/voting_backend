84483ead656ba58198ff48a2d4e8c83c15437e60
vk9769 <vaihhavkothare@gmail.com>
2025-10-12 11:14:57 +0530

    Fixed pool import/export mismatch

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 // controllers/authController.js
-import { pool } from '../db.js';
+import pool from '../db.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
